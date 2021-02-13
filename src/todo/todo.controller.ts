9e2b13eae58b4d4499e631149b074b2c60ca52f8
nour-beyrem <53348004+nour-beyrem@users.noreply.github.com>
2021-02-13 21:00:40 +0100

    crud todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { Todo } from './models/todo';
 import { v4 as uuidv4 } from 'uuid';
 import { TodoStatusEnum } from './enums/TodoStatusEnum';
@@ -29,4 +29,57 @@ export class TodoController {
       this.todos.push(todo);
       return todo;
     }
+
+    @Get(':id')
+  getTodoById(
+    @Param('id') id: string  
+  ): Todo   {
+   return  this.todos.find(
+       (todo : Todo) => todo.id === id);
+  }
+  
+
+  @Delete('delete/:id')
+  DeletById(
+    @Param('id') id:string)
+    : String {
+    const todo = this.todos.find((todo: Todo) => todo.id === id);
+    const todoId = this.todos.findIndex((todo: Todo) => todo.id === id);
+    if (todoId > 0) {
+      this.todos.splice(todoId, 1);
+      return 'le todo a ete supprimer avec succée ';
+    } else {
+      return  "le todo n existe pas";
+    }
+  }
+
+  @Put('update/:id')
+  ModifierTodo(@Param('id') id, 
+  @Body() todoData)
+  : Todo{
+    const todo = this.todos.find((todo : Todo) => todo.id === id);
+
+    const {name, description,date} = todoData;
+    
+      todo.name = name;
+      todo.description=description;
+    
+    return todo;
+
+  }
+  
+
+  @Patch('updatePartie/:id')
+  ModifierPartieTodo(@Param('id') id, @Body() todoData: Partial<Todo>) : Todo{
+ 
+    const todo = this.todos.find((todo : Todo) => todo.id === id);
+    const {name, description,date} = todoData;
+    if (todo.name != name){
+       todo.name=name;
+    }
+    if (todo.description != description){
+      todo.description = description;
+    }
+   return todo;
+  }
 }
